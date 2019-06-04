0a6b5d9ced4f8e9288215bdcfb094f18bbcc909a
mayabytatech <maya.k.k@lxisoft.com>
2019-06-04 12:56:39 +0530

    elastic config changed

--- src/main/java/com/diviso/graeshoppe/batch/DBWriter.java
+++ src/main/java/com/diviso/graeshoppe/batch/DBWriter.java
@@ -1,18 +1,18 @@
- /*
- * Copyright 2002-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+* Copyright 2002-2016 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.diviso.graeshoppe.batch;
 
 import java.util.List;
@@ -28,45 +28,51 @@ import com.diviso.graeshoppe.repository.ProductRepository;
 import com.diviso.graeshoppe.repository.StockCurrentRepository;
 import com.diviso.graeshoppe.service.dto.ProductDetailDTO;
 
-
 /**
- * TODO Provide a detailed description here 
- * @author MayaSanjeev
- * mayabytatech, maya.k.k@lxisoft.com
+ * TODO Provide a detailed description here
+ * 
+ * @author MayaSanjeev mayabytatech, maya.k.k@lxisoft.com
  */
 @Component
 public class DBWriter implements ItemWriter<ProductDetailDTO> {
 
-    @Autowired
-    private ProductRepository productRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-    @Autowired
-    StockCurrentRepository stockCurrentRepo;
-    
-    @Override
-    public void write(List<? extends ProductDetailDTO> products) throws Exception {
-    
-    		System.out.println("Data Saved for Users:>>>>>>>>>>" + products);
-    		for(ProductDetailDTO p:products){
-    			Product dto=new Product();
-    			dto.setId(p.getId());
-    			dto.setName(p.getName());
-    			dto.setSearchkey("");
-    			dto.setReference("");
-    			System.out.println("............... before save........... ");
-    		Product pro=	productRepository.save(dto);
-    		
-    		System.out.println("............... saved product:  "+pro);
-    		
-    			dto.setStockCurrent(new StockCurrent());
-    			
-    			dto.getStockCurrent().setSellPrice(p.getPrice());
-    			
-    			stockCurrentRepo.save(dto.getStockCurrent());
-    			
-    			System.out.println(".............. saved stock: "+dto.getStockCurrent());
-    		
-    		}
-    }
-       
-    }
+	@Autowired
+	StockCurrentRepository stockCurrentRepo;
+
+	@Override
+	public void write(List<? extends ProductDetailDTO> products) throws Exception {
+
+		System.out.println("Data Saved for Users:>>>>>>>>>>" + products);
+		for (ProductDetailDTO p : products) {
+			
+			Product dto = new Product();
+			
+			dto.setId(p.getId());
+			
+			dto.setName(p.getName());
+			
+			dto.setSearchkey("");
+			
+			dto.setReference("");
+			
+			System.out.println("............... before save........... ");
+			
+			Product pro = productRepository.save(dto);
+
+			System.out.println("............... saved product:  " + pro);
+
+			dto.setStockCurrent(new StockCurrent());
+
+			dto.getStockCurrent().setSellPrice(p.getPrice());
+
+			stockCurrentRepo.save(dto.getStockCurrent());
+
+			System.out.println(".............. saved stock: " + dto.getStockCurrent());
+
+		}
+	}
+
+}
--- src/main/java/com/diviso/graeshoppe/batch/LoadController.java
+++ src/main/java/com/diviso/graeshoppe/batch/LoadController.java
@@ -1,23 +1,29 @@
- /*
- * Copyright 2002-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+* Copyright 2002-2016 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package com.diviso.graeshoppe.batch;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.jfree.util.Log;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
@@ -29,43 +35,67 @@ import org.springframework.batch.core.repository.JobExecutionAlreadyRunningExcep
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
- * TODO Provide a detailed description here 
- * @author MayaSanjeev
- * mayabytatech, maya.k.k@lxisoft.com
+ * TODO Provide a detailed description here
+ * 
+ * @author MayaSanjeev mayabytatech, maya.k.k@lxisoft.com
  */
 @RestController
 @RequestMapping("/api")
 public class LoadController {
 
-    @Autowired
-    JobLauncher jobLauncher;
+	@Autowired
+	JobLauncher jobLauncher;
 
-    @Autowired
-    Job job;
+	@Autowired
+	Job job;
 
-    @GetMapping("/load-product")
-    public BatchStatus load() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+	@GetMapping("/load-product")
+	public BatchStatus load(/*@RequestPart("file") MultipartFile file*/) throws JobParametersInvalidException,
+			JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, IOException {
+		/*
+		// ....................file upload..................................
+		
+		byte[] bytes = file.getBytes();
+		Path path = Paths.get("src/main/resources/" + file.getOriginalFilename());
+		Files.write(path, bytes);*/
+		
+		
+		// ....................................................................................
+		Map<String, JobParameter> maps = new HashMap<>();
+		maps.put("time", new JobParameter(System.currentTimeMillis()));
+		JobParameters parameters = new JobParameters(maps);
 
+		JobExecution jobExecution = jobLauncher.run(job, parameters);
 
-        Map<String, JobParameter> maps = new HashMap<>();
-        maps.put("time", new JobParameter(System.currentTimeMillis()));
-        JobParameters parameters = new JobParameters(maps);
-        
-        JobExecution jobExecution = jobLauncher.run(job, parameters);
+		System.out.println("JobExecution: " + jobExecution.getStatus());
 
-        System.out.println("JobExecution: " + jobExecution.getStatus());
+		System.out.println("Batch is Running..." + jobExecution.isRunning());
+		while (jobExecution.isRunning()) {
+			System.out.println("...");
+		}
 
-        System.out.println("Batch is Running..."+jobExecution.isRunning());
-        while (jobExecution.isRunning()) {
-            System.out.println("...");
-        }
+		return jobExecution.getStatus();
+	}
 
-        return jobExecution.getStatus();
-    }
+	@PostMapping("/upload-file")
+	public void uploadFile(@RequestPart("file") MultipartFile file) throws IOException {
+		System.out.println("file        ............        :" + file);
+		byte[] bytes = file.getBytes();
+		Path path = Paths.get("src/main/resources/" + file.getOriginalFilename());
+		Files.write(path, bytes);
+	}
+	
+	
+	
 }
-
