0298ad46d3091a0b2e4ad5ec352bbcbaee05d441
mayabytatech <maya.k.k@lxisoft.com>
2019-06-28 15:22:30 +0530

    uncommented settings in product

--- src/main/java/com/diviso/graeshoppe/domain/Product.java
+++ src/main/java/com/diviso/graeshoppe/domain/Product.java
@@ -24,7 +24,7 @@ import java.util.Objects;
 @Entity
 @Table(name = "product")
 @Document(indexName = "product")
-//@Setting(settingPath = "settings/indexsettings.json")
+@Setting(settingPath = "settings/indexsettings.json")
 //@Mapping(mappingPath = "mappings/productmapping.json")
 public class Product implements Serializable {
 
